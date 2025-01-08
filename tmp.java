b3bc32f1f9730a188df7fd89cba140531b1a963f
Gautamverma2003 <gautam146official@gmail.com>
2025-01-08 11:11:18 +0530

    tmp in Fahrenite file

---
+++ tmp.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class tmp{
+public static void main(String []args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the temperature in celsius");
+
+ double celsius = sc.nextDouble();
+ double Fahrenheit= (celsius * 9/5) + 32;
+ System.out.println("temperature in Fahrenheit is " + Fahrenheit);
+ }
+ }
+ 
+ 
